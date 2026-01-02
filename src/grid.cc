56e6f4d4579da3027732dfa3f20c4a8eb39616f6
Monarch Kumar <monarchgam@gmail.com>
2026-01-02 01:32:45 +0000

    Added 2D Poisson code. Added better comments and improved layout.

--- src/grid.cc
+++ src/grid.cc
@@ -22,11 +22,12 @@ class Grid1D{
     struct simplexLineSegment{
         uint index;
         std::array<uint, 2> points;
+        double length;
     };
     std::vector<simplexLineSegment> simplices; //The list of simplex line segments in 1D grid.
     std::vector<point1D> boundarySimplices; //The list of boundary points in 1D grid.
 
-    //Make grid when initiated.
+    //Make grid when initiated. Uniform distribution.
     Grid1D(double xMin, double xMax, uint xNum){
         double xLen = xMax-xMin;
         
@@ -47,7 +48,7 @@ class Grid1D{
         uint index = 0;
         for(point1D p: points){
             if(p.x!=xMax){
-                simplices.push_back({index,p.index, p.index+1});
+                simplices.push_back({index,p.index,p.index+1,points[p.index+1].x-p.x});
                 index++;
             }
         }
@@ -65,13 +66,24 @@ class Grid1D{
         for (point1D p: points) {std::cout<<p.index<<"-"<<p.boundary<<"-> x:"<<p.x<<std::endl;}
     }
     void printSimplex(){
-        printf("Line Segment Simplices:[index-> 2x Point Indicies]\n");
-        for (simplexLineSegment s: simplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<std::endl;}    
+        printf("Line Segment Simplices:[index-> 2x Point Indicies, length]\n");
+        for (simplexLineSegment s: simplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<" len:"<<s.length<<std::endl;}    
     }
     void printBoundarySimplices(){
         printf("Boundary Point Simplices:[index-> x]\n");
         for (point1D s: boundarySimplices){std::cout<<s.index<<"-> x:"<<s.x<<std::endl;}    
     }
+    void output(std::ostream& output = std::cout){
+        //Prints the 'value' variable to the ostream item. Defaults to std::cout.
+        std::string row = "";
+        for (point1D p: Grid1D::points){
+            std::string cell =  std::format("{:.3e},",p.x);
+            if(cell[0]!='-') cell = ' ' + cell;
+            row+=cell;
+        }
+        row.pop_back();
+        output<<row<<std::endl;
+    } 
 };
 
 class Grid2D{
@@ -89,10 +101,12 @@ class Grid2D{
     struct simplexTriangle{
         uint index;
         std::array<uint, 3> points;
+        double area;
     };
     struct simplexLineSegment{
         uint index;
         std::array<uint, 2> points;
+        double length;
     };
     std::vector<simplexTriangle> simplices; //The list of simplex triangles in 2D grid.
     std::vector<simplexLineSegment> boundarySimplices; //The list of boundary simplex lines in 2D grid.
@@ -115,16 +129,29 @@ class Grid2D{
         calcSimplex(xMax,yMax,xNum,yNum);
         calcSimplexBoundary(xMax,yMax,xNum,yNum);
     }
-
     //Calculate and list simplices
     void calcSimplex(double xMax, double yMax, uint xNum, uint yNum){
         //If point2D's x or y at max do nothing; else, make two triangles point2D's x->x+1 and y-y+1 region.
         uint index = 0;
+        std::array<uint,3> set;
         for(point2D p: points){
             if(p.x!=xMax && p.y!=yMax){
                 simplices.push_back({index,p.index, p.index+yNum, p.index+1});
+                set = simplices.back().points;
+                simplices.back().area = 0.5 * std::abs(
+                 points[set[0]].x * (points[set[1]].y - points[set[2]].y) +
+                    points[set[1]].x * (points[set[2]].y - points[set[0]].y) +
+                    points[set[2]].x * (points[set[0]].y - points[set[1]].y)
+                ); 
                 index++;
-                simplices.push_back({index,p.index+1+yNum, p.index+1, p.index+yNum});
+
+                simplices.push_back({index,p.index+yNum+1, p.index+1, p.index+yNum});
+                set = simplices.back().points;
+                simplices.back().area = 0.5 * std::abs(
+                 points[set[0]].x * (points[set[1]].y - points[set[2]].y) +
+                    points[set[1]].x * (points[set[2]].y - points[set[0]].y) +
+                    points[set[2]].x * (points[set[0]].y - points[set[1]].y)
+                ); 
                 index++;
             }
         }
@@ -135,12 +162,12 @@ class Grid2D{
             if(p.boundary){ //If point2D is boundary, look x+1 and y+1 items; if both boundary add pair to boundarySimplices
                 if(p.y != yMax) {
                     if(p.boundary == points.at(p.index+1).boundary){
-                        boundarySimplices.push_back({static_cast<uint>(boundarySimplices.size()),p.index, p.index+1});
+                        boundarySimplices.push_back({static_cast<uint>(boundarySimplices.size()),p.index, p.index+1,std::abs(points[p.index+1].y-p.y)});
                     }
                 }
                 if(p.x != xMax) {
                     if(p.boundary == points.at(p.index+yNum).boundary){
-                        boundarySimplices.push_back({static_cast<uint>(boundarySimplices.size()),p.index, p.index+yNum});
+                        boundarySimplices.push_back({static_cast<uint>(boundarySimplices.size()),p.index, p.index+yNum,std::abs(points[p.index+yNum].x-p.x)});
                     }
                 }
             }             
@@ -151,13 +178,32 @@ class Grid2D{
         for (point2D p: points) {std::cout<<p.index<<"-"<<p.boundary<<"-> x:"<<p.x<<", y:"<<p.y<<std::endl;}
     }
     void printSimplex(){
-        printf("Triangular Simplices:[index-> 3x Point Indicies]\n");
-        for (simplexTriangle s: simplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<" "<<s.points[2]<<std::endl;}    
+        printf("Triangular Simplices:[index-> 3x Point Indicies, Area]\n");
+        for (simplexTriangle s: simplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<" "<<s.points[2]<<", "<<s.area<<std::endl;}    
     }
     void printBoundarySimplices(){
-        printf("Boundary Line Segment Simplices:[index-> 2x Point Indicies]\n");
-        for (simplexLineSegment s: boundarySimplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<std::endl;}    
+        printf("Boundary Line Segment Simplices:[index-> 2x Point Indicies, length]\n");
+        for (simplexLineSegment s: boundarySimplices){std::cout<<s.index<<"-> "<<s.points[0]<<" "<<s.points[1]<<" len:"<<s.length<<std::endl;}    
     }
+    void output(std::ostream& output = std::cout){
+        //Prints the 'points' variable to the ostream item. Defaults to std::cout.
+        for (int i = 0; i<Grid2D::points.size(); i++){
+            std::vector<std::string> rows(3);
+            for (auto val: Grid2D::points){
+                std::string index =  std::format("{},",val.index);
+                rows[0] = std::move(index);
+                std::string x =  std::format("{:.3e},",val.x);
+                if(x[0]!='-') x = ' ' + x;
+                rows[1] = std::move(x);
+                std::string y =  std::format("{:.3e},",val.y);
+                if(y[0]!='-') y = ' ' + y;
+                rows[2] = std::move(y);
+            }
+            rows[1].pop_back();
+            rows[2].pop_back();
+            for(auto row:rows){output<<row<<std::endl;}
+        }
+    } 
 };
 
 class Matrix1D{
@@ -169,15 +215,21 @@ class Matrix1D{
         std::vector<double> values(size, value);
         Matrix1D::values = std::move(values);
     }
-    Matrix1D(Grid1D grid){
+    Matrix1D(Grid1D grid1d){
         //Creates a matrix and calculates values based on slopes in 'grid' given. Indexes values according to 'grid' indices.  
-        std::vector<double> values(grid.points.size(), 0);
-        for (Grid1D::simplexLineSegment i: grid.simplices){
-            double h = grid.points[i.points.back()].x-grid.points[i.points.front()].x;
-            values[i.points.front()]+=0.5*h;
-            values[i.points.back()]+=0.5*h;
+        std::vector<double> values(grid1d.points.size(), 0);
+        for (Grid1D::simplexLineSegment i: grid1d.simplices){
+            values[i.points.front()]+=0.5*i.length;
+            values[i.points.back()]+=0.5*i.length;
         }
         Matrix1D::values = std::move(values);
+    }
+    //Finite elements : theory, fast solvers, and applications in solid mechanics by Braess, Dietrich, 2001, pg 54
+    //NOTE: Needs work
+    Matrix1D(Grid2D grid2d){
+        for(Grid2D::point2D p:grid2d.points){
+            Matrix1D::values.push_back(0);
+        }
     }
     void dirichlet(uint index, double value){values[index]=value;}
     void neumann(uint index, double value){values[index]+=value;}
@@ -204,6 +256,56 @@ class Matrix2D{
         std::vector<std::vector<double>> values(size, std::vector<double>(size,value));
         Matrix2D::values = std::move(values);
     }
+    Matrix2D(Grid2D grid2d){
+        // Creates a matrix and calculates values based on slopes in 'grid' given. Indexes values according to 'grid' indices. 
+        // Procedure:
+        // Loop through simplexes.
+        // Calculate gradients (ui/dx , ui/dy) for each point (i) in the simplex.
+        // Then we should have all variables for required equation LHS calculation.
+
+        std::vector<std::vector<double>> values(grid2d.points.size(), std::vector<double>(grid2d.points.size(),0));
+        for (Grid2D::simplexTriangle s: grid2d.simplices){
+
+            Grid2D::point2D p0 = grid2d.points[s.points[0]];
+            Grid2D::point2D p1 = grid2d.points[s.points[1]];
+            Grid2D::point2D p2 = grid2d.points[s.points[2]];
+            // slope in x = a & slope in y = b when we calculate
+            // u1   x1 y1 1   a
+            // u2 = x2 y2 1 X b
+            // u3   x3 y3 1   c
+
+            //ui values will change each point in simplex, will define later
+            Matrix2D pij(3);
+            pij.values={
+                {p0.x, p0.y, 1},
+                {p1.x, p1.y, 1},
+                {p2.x, p2.y, 1},
+            };
+            pij.inverse();
+
+            //Example
+            //ui.values = {0,1,0};
+            //std::vector<double> slope = pij.mul(ui).values;
+            //We get {del ui / del x, del ui / del y, dontcare} of simplex.
+
+            std::vector<std::vector<double>> slope;
+            for(uint idx =0; idx < s.points.size(); idx++){
+                // Get slope when each point is basis
+                Matrix1D ui(3);
+                ui.values[idx] = 1;
+                std::vector<double> slopeIdx = pij.mul(ui).values;
+                slope.push_back(slopeIdx);
+            }
+            for(uint i = 0;i <s.points.size();i++){
+                for(uint j = 0;j <s.points.size();j++){
+                    // OPERATION FOR POISSON 2D//
+                    // For Poisson we need, Int Int {(del ui / del x) + (del uj / del x)} * {(del ui / del j) + (del uj / del y)} dx dy
+                    values[s.points[i]][s.points[j]] += ((slope[i][0] * slope[j][0]) + (slope[i][1] * slope[j][1])) * (s.area);
+                }
+            }
+        }
+        Matrix2D::values = std::move(values);
+    }
     Matrix2D(Grid1D grid){
         //Creates a matrix and calculates values based on slopes in 'grid' given. Indexes values according to 'grid' indices.  
         //[ 1,-1] * 1/h
@@ -218,12 +320,27 @@ class Matrix2D{
         }
         Matrix2D::values = std::move(values);
     }
+    Matrix1D mul(Matrix1D mat){
+        uint size = mat.values.size();
+        Matrix1D res(size,0);
+        for(uint i = 0;i<size;i++){
+            for(uint j = 0;j<size; j++){
+                res.values[i]+=Matrix2D::values[i][j]*mat.values[j]; 
+            }
+        }
+        return res;
+    }
+    void boundary(void (*boundaryAssignment)(Grid2D grid)){
+
+    }
     void dirichlet(uint index, double value=0){
         std::vector<double> row(Matrix2D::values.size(),0);
         row[index] = 1;
         Matrix2D::values[index]=std::move(row);
     }
-    void neumann(uint index=0, double value=0){NULL;}
+    void neumann(uint index=0, double value=0){
+        NULL;
+    }
     void output(std::ostream& output = std::cout){
         //Prints the 'value' variable to the ostream item. Defaults to std::cout.
         for (int i = 0; i<Matrix2D::values.size(); i++){
@@ -237,8 +354,8 @@ class Matrix2D{
             output<<row<<std::endl;
         }
     } 
-    // https://www.math-cs.gordon.edu/courses/ma342/handouts/gauss.pdf
     void inverse(){
+    // https://www.math-cs.gordon.edu/courses/ma342/handouts/gauss.pdf
         uint n = Matrix2D::values.size();
 
         //Augment identity matrix
@@ -286,26 +403,6 @@ class Matrix2D{
         }
         Matrix2D::inverted= !Matrix2D::inverted;
     }
-    Matrix1D mul(Matrix1D mat){
-        uint size = mat.values.size();
-        Matrix1D res(size,0);
-        for(uint i = 0;i<size;i++){
-            for(uint j = 0;j<size; j++){
-                res.values[i]+=Matrix2D::values[i][j]*mat.values[j]; 
-            }
-        }
-        return res;
-    }
 };
 
 
-class AssmDelUDelV{
-    //Create assembled matrix for Del U . Del V operation.
-    public:   
-    enum dim {ONEDIM, TWODIM};
-
-        AssmDelUDelV(dim dimension){
-            
-
-        };
-};
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,12 @@
 #include <fstream>
 #include <iostream>
 #include <ostream>
+#include <vector>
 #include "version.cc"
 #include "grid.cc"
 
 using namespace std;
-#define NUMBER_OF_DASH 64
+#define NUMBER_OF_DASH 32
 #define DEBUG true
 
 string version = "0.1";
@@ -49,31 +50,73 @@ int main(){
     ///       Solving Poisson equation in 1-D        //
     ///////////////////////////////////////////////////
 
-    Grid1D base(0,1,10);
+    // Grid1D base(0,1,8);
 
-    Matrix1D f(base);
-    f.dirichlet(0, -1);
-    f.dirichlet(f.values.size()-1, 1.5);
+    // Matrix1D f1(base);
+    // f1.dirichlet(0, 0);
+    // f1.dirichlet(f1.values.size()-1, 0);
 
-    Matrix2D A(base);
-    A.dirichlet(0, -1);
-    A.dirichlet(f.values.size()-1, 1.5);
+    // Matrix2D A1(base);
+    // A1.dirichlet(0, 0);
+    // A1.dirichlet(f1.values.size()-1, 0);
 
-    //Doing inv(A)
-    A.inverse();
+    // //Doing inv(A)
+    // A1.inverse();
 
-    //Doing inv(A) * f
-    Matrix1D res = A.mul(f);
+    // //Doing inv(A) * f
+    // Matrix1D res1 = A1.mul(f1);
 
-    ofstream file("./output/res.csv");
-    res.output(file);
-    file.close();
+    // ofstream file1D("./output/res.csv");
+    // base.output(file1D);
+    // res1.output(file1D);
+    // file1D.close();
+
+    ///////////////////////////////////////////////////
+    ///       Solving Poisson equation in 2-D        //
+    ///////////////////////////////////////////////////
+    Grid2D base2D(0,1,0,1,3,3);
+    // base2D.printPoints();
+    // base2D.printSimplex();
+    // base2D.printBoundarySimplices();
+
+    Matrix1D f2(base2D);
+    Matrix2D A2(base2D);
+
+    //Applying Dirichlet Conditions at (0,0)
+    //Using u(x,y) = (x^2 + y^2) / 4 = 0
+    f2.values[base2D.points[0].index] = 0;
+    std::vector<double> temp(A2.values.size(),0);
+    A2.values[base2D.points[0].index] = temp;
+    A2.values[base2D.points[0].index][base2D.points[0].index] = 1;
+
+    //Applying Neumann Conditions at Boundaries
+    //Using u(x,y) = (x^2 + y^2) / 4 
+    for (Grid2D::point2D p: base2D.points){
+        if(p.boundary){
+            f2.values[p.index]={(p.x*p.x + p.y*p.y)/4.0};
+        }
+    }
+
+    A2.output(cout);
+    cout<<endl;
+    // f2.output(cout);
+    // cout<<endl;
+
+    //Solving
+    A2.inverse();
+    Matrix1D res2 = A2.mul(f2);
+
+    res2.output();
+    cout<<" NOTE: f matrix not defined correctly yet."<<endl;
 
     ///////////////////////////////////////////////////
     ///                  Next Steps                  //
     ///////////////////////////////////////////////////
 
-    //Make adjustments for 2D scenarios
+    // Fixing 2D Poisson.
+    // Adding more flexibility to equations (accessible to more than poisson equations.)
+    // Adding better documentation and README
+
 
     cout << string(NUMBER_OF_DASH, '-') << endl;
     cout << "No Errors. Exiting" << endl;
